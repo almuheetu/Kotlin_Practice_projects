926ef52d8d74be6ecaa0220c56cd025bfff36821
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-03 15:52:37 +0600

    Conditional statements setup

--- src/Main.kt
+++ src/Main.kt
@@ -2,23 +2,40 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main(args: Array<String>) {
 
-    Int
-    String
-    Boolean
-    Float
-    Double
-    Long
-    Char
-    Byte
-    Short
+//    Int
+//    String
+//    Boolean
+//    Float
+//    Double
+//    Long
+//    Char
+//    Byte
+//    Short
 
 //    var name : String = "Dev"
-    var myname = "Shihab"
-    var age : Int = 22
+//    var myname = "Shihab"
+//    var age : Int = 22
 
 
 //    println("Hello $myname, Welcome to kotlin World!")
-    println("Hello My name is $myname")
-    println("I am $age Years old")
+//    println("Hello My name is $myname")
+//    println("I am $age Years old")
 
+
+//    println("The sum is ${add(5,6)}")
+
+
+
+//    Conditional Statements
+
+    var num = 111
+
+    if (num>100){
+        println("No. is Greater")
+    } else {
+        println("No. is Smaller")
+    }
 }
+//fun add(a: Int, b: Int) : Any {
+//    return a+b
+//}
