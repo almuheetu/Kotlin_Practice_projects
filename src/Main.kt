39100fd223d5139599b3980762eedf5579fe44e3
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-06 09:26:40 +0600

    some problem issu fixed

--- src/Main.kt
+++ src/Main.kt
@@ -41,13 +41,14 @@ fun main(args: Array<String>) {
 
 
 
-    println("Hello My name is Shihab")
-
-    var num = 0
-
-    for (i in 0..10){
-        println("Number is: ${num++}")
-    }
+//    println("Hello My name is Shihab")
+//
+//    var num = 0
+//
+//    for (i in 0..10){
+//        println("Number is: ${num++}")
+//    }
+    println(args.contentToString())
 }
 //fun add(a: Int, b: Int) : Any {
 //    return a+b
